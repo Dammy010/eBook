6cd7ef0b1439193d8eef4e8740c891d808798597
Damilare <da9783790@gmil.com>
2025-05-15 12:12:24 +0100

    fixes

--- src/pages/Home/components/FeaturedProduct.jsx
+++ src/pages/Home/components/FeaturedProduct.jsx
@@ -1,33 +1,36 @@
-  import { useEffect, useState } from "react";
-  import { toast } from "react-toastify";
-  import { ProductCard } from "../../../Components/Elements/ProductCard";
-  import { getFeaturedList } from "../../../services/productService";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+// import { ProductCard } from "../../../Components/Elements/ProductCard";
+import { getFeaturedList } from "../../../services/productService";
 
-  export const FeaturedProducts = () => {
-    const [products, setProducts] = useState([]);
+export const FeaturedProducts = () => {
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-      async function fetchProducts(){
-        try{
-          const data = await getFeaturedList();
-          setProducts(data);
-        } catch(error){
-          toast.error(error.message, {closeButton: true, position: "bottom-center" });
-        }      
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const data = await getFeaturedList();
+        setProducts(data);
+      } catch (error) {
+        toast.error(error.message, {
+          closeButton: true,
+          position: "bottom-center",
+        });
       }
-      fetchProducts();
-    }, [])
+    }
+    fetchProducts();
+  }, []);
 
-    return (
-      <section className="my-20">
-          <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">Featured eBooks</h1>    
-          <div className="flex flex-wrap justify-center lg:flex-row">
-
-            { products.map((product) => (
+  return (
+    <section className="my-20">
+      <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">
+        Featured eBooks
+      </h1>
+      <div className="flex flex-wrap justify-center lg:flex-row">
+        {/* { products.map((product) => (
               <ProductCard key={product.id} product={product} />
-            )) }
-
-          </div>
-      </section>
-    )
-  }
+            )) } */}
+      </div>
+    </section>
+  );
+};
