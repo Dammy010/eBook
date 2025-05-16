b1c7bac54878c704bfa8b0e5d69564a8aac88f29
Damilare <da9783790@gmil.com>
2025-05-16 09:10:11 +0100

    console.log added for debugging

--- src/pages/Home/components/FeaturedProduct.jsx
+++ src/pages/Home/components/FeaturedProduct.jsx
@@ -21,6 +21,8 @@ export const FeaturedProducts = () => {
     fetchProducts();
   }, []);
 
+  console.log({products})
+
   return (
     <section className="my-20">
       <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">
